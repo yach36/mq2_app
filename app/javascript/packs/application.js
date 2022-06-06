461829adae834d76048667f438db1b559150cbe1
yach36 <ubuntu@ip-172-31-9-65.us-west-2.compute.internal>
2022-06-06 05:04:10 +0000

    Animated footer pointer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("jquery-easing");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,17 +19,34 @@ require("jquery")
 // const imagePath = (name) => images(name, true)
 
 $(document).on("turbolinks:load", function() {
-  // headerのボタン動作
+  /* headerのボタン動作 */
+  // クリックするごとにクラスを取り外しする
   const $hamburgerBtn = $(".js_hamburger");
   const $hamburgerBar = $(".js_hamburger-bar");
   $hamburgerBtn.on("click", function() {
     $(this).children($hamburgerBar).toggleClass("is_active");
   });
   
-  // footerのボタン動作
+  /* footerのボタン動作 */
+  // ポインタの位置の初期化
+  const $pointer = $(".js_footer_item-pointer");
+  const firstPos = $("#js_footer_item-1").offset();
+  const firstWidth = $("#js_footer_item-1").width();
+  $pointer.css({
+    top: 0,
+    left: firstPos.left + firstWidth / 2
+  });
+  
+  // クリックするとその要素をポイントする
   const $footerItem = $(".js_footer_item");
   $footerItem.on("click", function() {
-    $footerItem.not(this).removeClass("is_active");
-    $(this).addClass("is_active");
+    const itemPos = $(this).offset();
+    const itemWidth = $(this).width();
+    $pointer.animate({
+      top: 0,
+      left: itemPos.left + itemWidth / 2
+    },
+    250,
+    "easeOutQuart");
   });
 });
