e98d83ff5c0b72c1574675c1710c72b4de95a2d8
yach36 <yach0306@gmail.com>
2022-06-10 07:31:56 +0000

    曲名検索結果を非同期で表示する機能を追加(posts/_new.html.erb, new.js.erb)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,8 @@
 class PostsController < ApplicationController
   # spotifyAPIの認証
   RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+  
   def index
-    # アーティスト名で曲を検索(rspotify)
-    if params[:search].present?
-      # 検索文字列にヒットする曲一覧
-      @tracks = RSpotify::Track.search(params[:search])
-      respond_to do |format|
-        format.html { redirect_to :root }
-        format.json { render json: @tracks }
-      end
-    end
-    
-    # フッターのカーソルを制御
-    # @cursorNum = params[:cursor]
-    # if @cursorNum.present?
-    #   respond_to do |format|
-    #     format.html
-    #     format.json { render json: @cursorNum }
-    #   end
-    # end
     @templateNum = 1
     respond_to do |format|
       format.html { render "posts/index" }
@@ -28,6 +11,15 @@ class PostsController < ApplicationController
   end
   
   def new
+    # アーティスト名で曲を検索(rspotify)
+    @search = params[:search]
+    if @search.present?
+      # 検索文字列にヒットする曲一覧
+      @tracks = RSpotify::Track.search(@search)
+      respond_to do |format|
+        format.json { render json: @tracks }
+      end
+    end
     respond_to do |format|
       format.html { render "posts/new" }
       format.js
